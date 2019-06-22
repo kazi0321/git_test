3ae8306d34ed6bf62e808645995698681d783183
tama0 <tama0@personal-pc>
2019-06-22 19:16:44 +0900

    first commit

---
+++ src/test/Main.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Seito seitoA = new Seito("seitoA",40);
+		Seito seitoB = new Seito("seitoB",50);
+		Seito seitoC = new Seito("seitoC",80);
+		Seito seitoD = new Seito("seitoD",90);
+		Seito seitoE = new Seito("seitoE",70);
+	}
+
+}
